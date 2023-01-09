8f1ffc60120cb82a0fbe78786579b248c3094979
Gleb Kikot <glebkikot200@gmail.com>
2023-01-09 23:04:13 +0200

    change

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -289,7 +289,7 @@ resource "aws_ecs_service" "main" {
  name                               = "${var.name}-service-${var.environment}"
  cluster                            = aws_ecs_cluster.main.id
  task_definition                    = aws_ecs_task_definition.main.arn
- desired_count                      = 2
+ desired_count                      = 1
  deployment_minimum_healthy_percent = 50
  deployment_maximum_percent         = 200
  launch_type                        = "FARGATE"
